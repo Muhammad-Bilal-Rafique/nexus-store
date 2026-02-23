cd3b7d717fb821af1deb43b3f3159d6b118139d5
Muhammad Bilal Rafique <bilalrafique275@gmail.com>
2026-02-23 10:32:33 +0500

    metadata updated

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 
 import Dashboard from "./Home/page.jsx";
 
+export const metadata = {
+  title: 'Nexus Store | Home',
+  description: 'Nexus Store - Your Online Streetwear Destination',
+}
+
 export default function Home() {
 
   return (
